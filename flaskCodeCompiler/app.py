f3d03e4d02a90fef93c8240b0821a16196441a7b
Shashikant Dwivedi <shashikant.dwivedi@skdblog.xyz>
2019-10-08 19:06:46 +0530

    DOSEN'T reminde

--- flaskCodeCompiler/app.py
+++ flaskCodeCompiler/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
 @app.route('/')
@@ -8,5 +12,15 @@ def home():
     return render_template('index.html')
 
 
+@socketio.on('run_code')
+def run_code(data):
+    print(data)
+
+
+@socketio.on('submit_code')
+def submit_code(data):
+    print(data)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
